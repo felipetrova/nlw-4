957395e193733d71323cb1c5c03bd2eebe457290
Felipe <felipee.trova@gmail.com>
2021-02-27 17:18:53 -0300

    Create functions and methods to send mail

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
+
 const userController = new UserController();
-const surveyController = new SurveyController;
+const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.get("/users", userController.show);
 router.post("/users", userController.create);
 
 router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
